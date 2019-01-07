694e784fe37fd637d754f7391088e8671ce4372f
Bijay Shrestha <8ijay.shrestha@gmail.com>
2019-01-07 21:16:59 +0545

    Update WebConfig.java

--- CRUD/src/main/java/com/thapa/crud/WebConfig.java
+++ CRUD/src/main/java/com/thapa/crud/WebConfig.java
@@ -31,3 +31,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     }
 
 }
+
+//Two things: First, I suggest you mention the reason why you'd use the afforementioned annotations.
+//For example, why is it necessary to use @ComponentScan for this particular class or say why @Bean to create and 
+//return a CrudController object or say why @Configuration or @EnableWebMvc anotations?
+//Second, the artifact/ code you've created would be better documented than usually found if you can
+// explain as to why you'd use 'ViewResolver' bean in this Adapter class?
